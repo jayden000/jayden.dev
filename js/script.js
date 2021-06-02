02510e44de435dddcf88026206e260e3af5d32f2
Jayden <jayden@jayden.dev>
2021-06-02 09:58:08 -0400

    feat: i fixed it :)

--- js/script.js
+++ js/script.js
@@ -1,45 +1,30 @@
 function setTheme(name) {
   localStorage.setItem("j", name);
+  // don't judge i know this is very janky but shut up it works and i fixed my problem - j 2021-06-02
+  let text = "lol";
+  if (name === "light")
+    text = "<a href='" + "javascript:toggleTheme()" + "'>click me :)</a>";
+  if (name === "dark")
+    text = "<a href='" + "javascript:toggleTheme()" + "'>don't click me :(</a>";
   document.documentElement.className = name;
+  document.getElementById("switcher").innerHTML = text;
 }
 
 function toggleTheme() {
   if (localStorage.getItem("j") === "dark") {
-    // prettier-ignore
-    document.getElementById("switcher").innerHTML = "<a href='" + "javascript:toggleTheme()" + "'>don't click me :(</a>";
     setTheme("light");
     console.log("set theme to light, you weirdo :(");
   } else if (localStorage.getItem("j") === "light") {
-    // prettier-ignore
-    document.getElementById("switcher").innerHTML = "<a href='" + "javascript:toggleTheme()" + "'>click me :)</a>";
     setTheme("dark");
     console.log("set theme to dark, you are normal :)");
   }
 }
 
-/*function j() {
-  if (localStorage.getItem("j") === "light") {
-    setTheme("light");
-    //prettier-ignore
-    document.documentElement.getElementById("switcher").innerHTML = "hi"
-    console.log("auto applied light :(");
-  } else {
-    setTheme("dark");
-    // prettier-ignore
-    document.getElementById("switcher").innerHTML = "not hi"
-    console.log("auto applied dark :)");
-  }
-}*/
-
 window.onload = function () {
   if (localStorage.getItem("j") === "light") {
-    // prettier-ignore
-    document.getElementById("switcher").innerHTML ="<a href='" + "javascript:toggleTheme()" + "'> click me :)</a>";
     setTheme("light");
     console.log("auto applied light :(");
   } else {
-    // prettier-ignore
-    document.getElementById("switcher").innerHTML = "<a href='"+"javascript:toggleTheme()"+"'>don't click me :(</a>" ;
     setTheme("dark");
     console.log("auto applied dark :)");
   }
