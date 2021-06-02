5fba637e54e56e79cf052b498e8a6f4ea343851e
Jayden <jayden@jayden.dev>
2021-06-02 09:38:42 -0400

    feat: added changing footer with theme (kinda)
    has a bug where for the first page load it says one of the footers twice in a row

--- js/script.js
+++ js/script.js
@@ -5,22 +5,42 @@ function setTheme(name) {
 
 function toggleTheme() {
   if (localStorage.getItem("j") === "dark") {
+    // prettier-ignore
+    document.getElementById("switcher").innerHTML = "<a href='" + "javascript:toggleTheme()" + "'>don't click me :(</a>";
     setTheme("light");
     console.log("set theme to light, you weirdo :(");
-  } else {
+  } else if (localStorage.getItem("j") === "light") {
+    // prettier-ignore
+    document.getElementById("switcher").innerHTML = "<a href='" + "javascript:toggleTheme()" + "'>click me :)</a>";
     setTheme("dark");
     console.log("set theme to dark, you are normal :)");
   }
 }
 
-function j() {
+/*function j() {
   if (localStorage.getItem("j") === "light") {
     setTheme("light");
+    //prettier-ignore
+    document.documentElement.getElementById("switcher").innerHTML = "hi"
     console.log("auto applied light :(");
   } else {
     setTheme("dark");
+    // prettier-ignore
+    document.getElementById("switcher").innerHTML = "not hi"
     console.log("auto applied dark :)");
   }
-}
+}*/
 
-window.onload = j();
+window.onload = function () {
+  if (localStorage.getItem("j") === "light") {
+    // prettier-ignore
+    document.getElementById("switcher").innerHTML ="<a href='" + "javascript:toggleTheme()" + "'> click me :)</a>";
+    setTheme("light");
+    console.log("auto applied light :(");
+  } else {
+    // prettier-ignore
+    document.getElementById("switcher").innerHTML = "<a href='"+"javascript:toggleTheme()"+"'>don't click me :(</a>" ;
+    setTheme("dark");
+    console.log("auto applied dark :)");
+  }
+};
