6ce953d6f5b381b755c3c21016085fdc1b429cf4
Jayden <jaydene1230@gmail.com>
2021-06-12 06:34:01 -0400

    feat: add changing favicon depending on theme

--- js/script.js
+++ js/script.js
@@ -1,13 +1,19 @@
+//i know this whole js file is janky pls don't call me out for being a bad dev :(
+
 function setTheme(name) {
   localStorage.setItem("j", name);
   // don't judge i know this is very janky but shut up it works and i fixed my problem - j 2021-06-02
   let text = "lol";
-  if (name === "light")
+  if (name === "light") {
     text = "<a onCLick='" + "javascript:toggleTheme()" + "'>click me :)</a>";
+    document.querySelector("link[rel='shortcut icon']").href = "./img/j.png";
+  }
 
-  if (name === "dark")
+  if (name === "dark") {
     text =
       "<a onClick='" + "javascript:toggleTheme()" + "'>don't click me :(</a>";
+    document.querySelector("link[rel='shortcut icon']").href = "./img/j-2.png";
+  }
 
   document.documentElement.className = name;
   document.getElementById("switcher").innerHTML = text;
